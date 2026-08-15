46dbd5de4b78ba9a82e517405170773117b5d2f4
demilord001 <demilord001@gmail.com>
2026-08-15 18:19:06 +0100

    feat: add @vercel/analytics for enhanced analytics tracking in the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs"
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -27,6 +28,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <ClerkProvider>
+    <Analytics />
       <html lang="en" className={cn(inter.variable, geistSans.variable, geistMono.variable)}>
         <body className="bg-background font-sans antialiased">
           {children}
